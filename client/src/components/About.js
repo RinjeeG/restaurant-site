8b85e6782273100ef5df3ff8ec0f8c4fa2d45034
Rinjee <rinjigelu39@gmail.com>
2024-12-06 11:01:22 -0700

    updated about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import coverPhoto from '../assets/images/cover.jpg';
 
 const About = () => {
   return (
@@ -8,18 +9,18 @@ const About = () => {
         <div className="col-md-6">
           <h3 className="mb-3">Our Story</h3>
           <p>
-            Welcome to [Restaurant Name], where we serve delicious food with a focus on fresh ingredients and 
-            an unforgettable dining experience. Established in [Year], we have been providing great food and 
+            Welcome to Limbu Kitchen, where we serve delicious food with a focus on fresh ingredients and 
+            an unforgettable dining experience. Established in 2024, we are committed to provide great food and 
             excellent service to our guests.
           </p>
           <p>
-            Our restaurant is known for its [specialty dish], and we take pride in the quality and flavors of 
+            Our restaurant is known for its cultural dishes , and we take pride in the quality and flavors of 
             every meal we prepare. Join us for a culinary journey where tradition meets innovation.
           </p>
         </div>
         <div className="col-md-6">
           <img 
-            src="https://via.placeholder.com/500x300"  // Replace with actual image URL
+            src= {coverPhoto} // Replace with actual image URL
             alt="Restaurant"
             className="img-fluid rounded"
           />
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,8 +7,8 @@ const Footer = () => (
       <img src={logo} alt="Limbu Kitchen Logo" style={{ height: '50px' }} />
       <p className="mt-3">Authentic Nepali Taste</p>
       <ul className="list-inline">
-        <li className="list-inline-item"><a href="#" className="text-white">Instagram</a></li>
-        <li className="list-inline-item"><a href="#" className="text-white">Facebook</a></li>
+        <li className="list-inline-item"><a href="https://www.instagram.com/limbu_kitchen/" className="text-white">Instagram</a></li>
+        <li className="list-inline-item"><a href="https://www.facebook.com/profile.php?id=61566837914772" className="text-white">Facebook</a></li>
       </ul>
       <p className="mt-3">© 2024 Limbu Kitchen. All rights reserved.</p>
     </div>
